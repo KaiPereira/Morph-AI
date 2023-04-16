91a279fd089d53f21fcbb51c57aaa9eca66b0fcf
Kai Pereira <kaipereira2020@gmail.com>
2023-04-16 13:03:31 -0700

    backup

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,9 @@ const cookieParser = require('cookie-parser');
 const app = express()
 app.use(cors({
     origin: process.env.ORIGIN,
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json())
 app.use(cookieParser());
