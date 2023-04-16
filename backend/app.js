6189b39118550006059f3421fdd50820288bde95
Kai Pereira <kaipereira2020@gmail.com>
2023-04-16 11:44:38 -0700

    added origin as env

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const cookieParser = require('cookie-parser');
 // Our MIDDLEware
 const app = express()
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.ORIGIN,
     credentials: true
 }));
 app.use(express.json())
