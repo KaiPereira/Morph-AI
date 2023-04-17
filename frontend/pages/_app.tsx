a37b2d79fcf66acc38f07556a42cc9e0f4666ddf
Kai Pereira <kaipereira2020@gmail.com>
2023-04-17 08:08:42 -0700

    google analytics

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -35,6 +35,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="description" content="Unlock your AI/ML programming potential with Morph, an innovative education platform that offers hands-on projects, project-based courses, and the latest AI/ML libraries. From beginners to experienced programmers, Morph helps learners develop skills at their own pace and build confidence through practical application. Join Morph today and achieve your AI/ML programming goals!" />
         <meta name="keywords" content="AI/ML education, innovative learning platform, hands-on projects, project-based courses, AI/ML libraries, AI/ML languages, learn from scratch, self-paced learning, practical application of AI/ML, confidence building, beginner-friendly programming, experienced programmer training, Morph education platform, AI/ML programming skills, achieve your programming goals" />
         <meta name="author" content="Kai Pereira" />
+        <meta name="google-site-verification" content="7unymZJOcMHrEjiCh1qz8oudMsDd0K40Z_bF3HDHYWM" />
         <title>Morph AI - Learn AI/ML Programming</title>
       </Head>{ !loading &&
         <Component {...pageProps} 
